3c96ab2e68a67c813ddcfd399c87c16e7983bd66
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2016-01-20 20:21:42 +0200

    Ready to start

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name wikiViewerApp
+ * @description
+ * # wikiViewerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('wikiViewerApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wikiViewerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wikiViewerApp
+ */
+angular.module('wikiViewerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
