c2311f757ceee3f45210105101fe46deb32cdda5
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2016-02-01 17:59:01 +0200

    Fix bug with displaying resultList

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,16 +10,20 @@
 var app = angular.module('wikiViewerApp');
 
 app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
-    $scope.resultList = [];
 
+    $scope.resultList = [];
     $scope.search = '';
 
     $scope.getResults = function () {
+        console.log("Start searching " + $scope.search);
         $.getJSON('https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro=&explaintext=&exsentences=1&exlimit=max&gsrsearch='+
         $scope.search +
         '&callback=\?', function(res){
-              $scope.resultList = res.query.pages;
-          });
+            $scope.resultList = res.query.pages;
+            $scope.$apply();
+        }).error(function (err) {
+              console.log(err);
+        });
     };
 
     $scope.checkIfEnterKeyWasPressed = function($event){
