daa5b0cff69608b48459c1aec619468d8bdc1733
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2016-02-02 20:58:48 +0200

    Add error msg and modify some css

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,17 +13,35 @@ app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
     $scope.resultList = [];
     $scope.search = '';
+    $scope.marginTop = 'marginTopBefore'
 
     $scope.getResults = function () {
-        console.log("Start searching " + $scope.search);
+
         $.getJSON('https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro=&explaintext=&exsentences=1&exlimit=max&gsrsearch='+
         $scope.search +
         '&callback=\?', function(res){
-            $scope.resultList = res.query.pages;
+            if (res.query) {
+              $scope.resultList = res.query.pages;
+          } else {
+              notie.alert(3, "Not found :( Try something else.", 3);
+          }
+            var myArray = $.map($scope.resultList, function(value, index) {
+                return value;
+            });
+
+            if (myArray.length > 0) {
+                $scope.marginTop = 'marginTopAfter';
+            } else {
+                $scope.marginTop = 'marginTopBefore';
+                console.log("Not found");
+            }
+
             $scope.$apply();
+
         }).error(function (err) {
-              console.log(err);
+              notie.alert(3, "Oops... Try again later.", 3);
         });
+
     };
 
     $scope.checkIfEnterKeyWasPressed = function($event){
